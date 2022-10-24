ce7c4b244c993fdeea05e192412cb884d38240a8
Guilherme Rodz <gui.rodz.dev@gmail.com>
2022-10-24 18:53:23 +0200

    refactor: use ids on `react.spec.tsx`

--- lib/tests/react.spec.tsx
+++ lib/tests/react.spec.tsx
@@ -16,37 +16,48 @@ const shareableConfig = cs({
   compoundVariants: [{ color: 'blue', className: 'compound' }],
 })
 
+let lastId = 0
+const newId = () => `test-id-${++lastId}`
+
 describe('React classnames', () => {
   test('class builder only', () => {
-    const getClasses = cb(shareableConfig)
-    render(<div id="1" className={getClasses()} />)
+    const id = newId()
 
-    const el = document.getElementById('1')
+    const getClasses = cb(shareableConfig)
+    render(<div id={id} className={getClasses()} />)
+
+    const el = document.getElementById(id)
     expect(el?.className).toBe('base base-two red')
   })
 
   describe('Styled components', () => {
     describe('Nesting', () => {
-      test('first depth', () => {
-        const Div = styled('div', shareableConfig)
-        render(<Div id="2" />)
+      test('1st depth', () => {
+        const id = newId()
 
-        const el = document.getElementById('2')
+        const Div = styled('div', shareableConfig)
+        render(<Div id={id} />)
+
+        const el = document.getElementById(id)
         expect(el?.className).toBe('base base-two red')
       })
 
-      test('third depth', () => {
+      test('3rd depth', () => {
+        const id1 = newId()
+        const id2 = newId()
+        const id3 = newId()
+
         const Div = styled('div', shareableConfig)
         const NestedDiv = styled(Div, shareableConfig)
         const NestedNestedDiv = styled(NestedDiv, shareableConfig)
 
-        render(<Div variants={{ color: 'green' }} id="3" />)
-        render(<NestedDiv variants={{ color: 'green' }} id="4" />)
-        render(<NestedNestedDiv variants={{ color: 'blue' }} id="5" />)
+        render(<Div variants={{ color: 'green' }} id={id1} />)
+        render(<NestedDiv variants={{ color: 'green' }} id={id2} />)
+        render(<NestedNestedDiv variants={{ color: 'blue' }} id={id3} />)
 
-        const el1 = document.getElementById('3')
-        const el2 = document.getElementById('4')
-        const el3 = document.getElementById('5')
+        const el1 = document.getElementById(id1)
+        const el2 = document.getElementById(id2)
+        const el3 = document.getElementById(id3)
 
         expect(el1?.className).toBe(el2?.className)
         expect(el3?.className).not.toBe(el2?.className)
@@ -55,10 +66,12 @@ describe('React classnames', () => {
 
     describe('when the final className is empty', () => {
       test('prevent empty `class` attribute in HTML element', () => {
-        const Div = styled('div', { base: ['', [''], ''] })
-        render(<Div id="6" />)
+        const id = newId()
 
-        const el1 = document.getElementById('6')
+        const Div = styled('div', { base: ['', [''], ''] })
+        render(<Div id={id} />)
+
+        const el1 = document.getElementById(id)
       })
     })
   })
